cf4d0693915d74d9f9d3739a5a099abf4515c8d9
Luke Mirman <orangejuicepanda1@gmail.com>
2026-06-25 18:41:11 -0400

    Fixed glyph manager not polling when paused (GH-20)
    Also snuck in extremely minor formatting changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this package are documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [2.2.3] - UNRELEASED
+
+### Fixed
+
+- Fixed RewiredGlyphManager.cs poll rate being affected by time scale ([GH-20](https://github.com/Orange-Panda/Rewired-Glyphs/issues/20))
+
 ## [2.2.2] - 2025-02-09
 
 ### Fixed
--- Runtime/API/RewiredGlyphManager.cs
+++ Runtime/API/RewiredGlyphManager.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 namespace LMirman.RewiredGlyphs
 {
 	/// <summary>
-	/// Responsible for initializing the <see cref="InputGlyphs"/> system and running <see cref="InputGlyphs"/> functionality through built in Unity functions.
+	/// Responsible for initializing the <see cref="InputGlyphs"/> system and running <see cref="InputGlyphs"/> functionality through built-in Unity functions.
 	/// </summary>
 	/// <remarks>
 	/// You must add this component to your input manager for <see cref="InputGlyphs"/> system to fully function.
@@ -15,20 +15,26 @@ namespace LMirman.RewiredGlyphs
 	[AddComponentMenu("Rewired Glyphs/Rewired Glyph Manager")]
 	public class RewiredGlyphManager : MonoBehaviour
 	{
-		[SerializeField, Tooltip("The default collection of glyphs to use in the Rewired Input Glyph system when initialized.")]
+		[Tooltip("The default collection of glyphs to use in the Rewired Input Glyph system when initialized.")]
+		[SerializeField]
 		private GlyphCollection glyphCollection;
-		[SerializeField, Tooltip("Additional glyph collections to load on start which can be referenced via their collectionKey.")]
+		[Tooltip("Additional glyph collections to load on start which can be referenced via their collectionKey.")]
+		[SerializeField]
 		private GlyphCollection[] additionalCollections = { };
-		[SerializeField, Tooltip("The player ids to check input changes for rebuilding glyphs on hardware changes.")]
+		[Tooltip("The player ids to check input changes for rebuilding glyphs on hardware changes.")]
+		[SerializeField]
 		private int[] playerIds = { 0 };
-		[SerializeField, Tooltip("How frequently this manager will poll the device list all observed `playerIds` to check for hardware changes."), Range(0, 5)]
+		[Tooltip("How frequently this manager will poll the device list all observed `playerIds` to check for hardware changes.")]
+		[Range(0, 5)]
+		[SerializeField]
 		private float hardwareChangePollingRate = 0.5f;
 
-		private float timer;
+		private float lastEvaluateTime;
 		private readonly List<ObservedPlayer> observedPlayers = new List<ObservedPlayer>();
 
 		private void Awake()
 		{
+			lastEvaluateTime = -hardwareChangePollingRate;
 			if (glyphCollection == null)
 			{
 				Debug.LogError($"No default GlyphCollection defined on \"{gameObject.name}\". Please add a Glyph Collection on the Rewired Glyph Manager.", gameObject);
@@ -68,13 +74,14 @@ namespace LMirman.RewiredGlyphs
 
 		private void CheckForHardwareChanges()
 		{
-			if (timer > 0)
+			float evaluationTimer = Mathf.Abs(Time.unscaledTime - lastEvaluateTime);
+			bool hasRecentlyEvaluated = evaluationTimer < hardwareChangePollingRate;
+			if (hasRecentlyEvaluated)
 			{
-				timer -= Time.deltaTime;
 				return;
 			}
 
-			timer = hardwareChangePollingRate;
+			lastEvaluateTime = Time.unscaledTime;
 			foreach (ObservedPlayer observedPlayer in observedPlayers)
 			{
 				Controller recentController = observedPlayer.player.controllers.GetMostRecentController();
